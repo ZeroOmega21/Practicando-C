91c9801186f56073dc96675f81b82dc1fa8881d4
ZeroOmeha21 <zero213omega@gmail.com>
2025-08-07 23:57:44 -0400

    Reflexion 1

--- Conversion de tipos de datos.c
+++ Conversion de tipos de datos.c
@@ -5,11 +5,12 @@ int main()
     //realizemos la suma de flotantes
     float f = 8.5891;
     float suma = (float)a + f;
-    printf("entero %i\n flotante %.2f\n double %.3f\n caracter %c\n", 
+    printf("entero %i\n flotante %.2f\n double %.3f\n caracter %c\n flotante a entero %d\n", 
         a,
         (float)a, // lo transformamos en flotante
         (double)a,
-        (char)a //lo que imprime son el caracte que representa el numero 23 = ↨
+        (char)a, //lo que imprime son el caracte que representa el numero 23 = ↨
+        (int)f
     );
     printf("Suma de %i y %.2f es igual a %.2f", a, f, suma);
     ;
